f14f329eb97c8ec563621902862ff8c402968da7
Eduardo Zimermam <eduardozimerman@live.com>
2018-08-29 16:28:58 -0300

    Adição de arquivos

---
+++ SistemasLineares.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+#include "utils.h"
+#include "SistemasLineares.h"
+
+// Método de Jacobi
+double  jacobi (double  *A, double *B,  double *X,  int n,
+		double *tIteracao, double *tTotal)
+{
+
+}
+
+// Método de Gauss-Seidel
+double  gaussSeidel (double  *A, double *B,  double *X,  int n,
+		     double *tIteracao, double *tTotal)
+{
+
+}
+
+
